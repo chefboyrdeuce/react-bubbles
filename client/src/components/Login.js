dfa6ccff2fd8de692185331c09a1289f2cf7da53
chefboyrdeuce <jpldeuce@gmail.com>
2020-01-27 19:31:45 -0500

    constructed axios request to retreive token and save to local storage, built axiosWithAuth module

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+
+import axios from 'axios';
+import api from '../utils/axiosAuth'
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+    const [error, setError] = useState()
+    const [data, setData] = useState({
+        username: "",
+        password: "",
+    })
+
+    const handleChange = (event) => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        api()
+            .post("api/login", data)
+            .then(result => {
+                console.log(result.data)
+                localStorage.setItem("token", result.data.payload)
+                console.log(localStorage)
+            })
+            .catch(error => {
+                setError(error.response.data)
+            })
+    }
+
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+        <form onSubmit={handleSubmit}>
+            {error && <div className="error">{error}</div>}
+            <input type="text" name="username" placeholder="Username" value={data.username} onChange={handleChange}/>
+            <input type="password" name="password" placeholder="Password" value={data.password} onChange={handleChange}/>
+            <button type="submit">Sign In</button>
+        </form>
     </>
   );
 };
